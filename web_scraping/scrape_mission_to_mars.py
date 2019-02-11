b9a1a87b7769a0b56a3b246908b2fce6034c4b13
Kaushik Samaddar <kaushiksamaddar.gmail.com>
2019-02-11 07:44:36 -0600

    Updated Mars Scrape Python script.

--- web_scraping/scrape_mission_to_mars.py
+++ web_scraping/scrape_mission_to_mars.py
@@ -44,16 +44,86 @@ def scrape():
     base_url = image_url.rsplit('/',2)[0]
 
     #get image url using BeautifulSoup
-    html_image = browser.html
-    soup = BeautifulSoup(html_image, "html.parser")
-    print("soup: %s" %soup)
-    img_url = soup.find("img", class_="fancybox-image")['src']
-    featured_image_url = base_url + img_url
+    # html_image = browser.html
+    # soup = BeautifulSoup(html_image, "html.parser")
+    # print("soup: %s" %soup)
+    # img_url = soup.find("img", class_="fancybox-image")['src']
+    # featured_image_url = base_url + img_url
+
+    #Mars weather
+    #Use Splinter to visit Mars URL.
+    mars_url = 'https://twitter.com/marswxreport?lang=en'
+    browser.visit(mars_url)
+
+    #Construct a Soap object on the Mars URL
+    html_mars = browser.html
+    soup_mars = BeautifulSoup(html_mars, 'html.parser')
+
+    #Find the weather tweet
+    weather_tweet = soup_mars.find('p', class_='TweetTextSize TweetTextSize--normal js-tweet-text tweet-text')
+    mars_facts["weathertweet"]=weather_tweet.text
+
+    #Scrape Mars Facts URL
+    mars_facts_url = 'https://space-facts.com/mars/'
+
+    #Create a Pandas Dataframe on the URL
+    mars_facts_df = pd.read_html(mars_facts_url)
+    mars_facts_df = mars_facts_df[0]
+
+    #Supplement Facts and Values as 2 columns to hold titles and values.
+    mars_facts_df.columns = ['Facts','Values']
+
+    #Convert the DataFrame to an HTML table of type String
+    mars_facts_df_table = mars_facts_df.to_html()
+    mars_facts["table"] = mars_facts_df_table.replace('\n','')
+    
+    #Mars hemisphere
+    url_hemisphere = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+
+    #Base URL
+    base_url_hemisphere = url_hemisphere.rsplit('/',2)[0]
+
+    #Request and Receive the Hemisphere Response Page
+    hemisphere_response = requests.get(url_hemisphere)
+
+    #Create a Beautiful Soap on the response text and parse the html
+    hemisphere_soap = BeautifulSoup(hemisphere_response.text, 'html.parser')
+
+    #Search for all the Image Items - Tag div and class item
+    imageItems = hemisphere_soap.find_all('div', class_='item')
+
+    #Construct a Hemisphere List
+    hemisphere_image_urls = []
+
+    '''
+    Iterate the Image Items to find all the product links and title.
+    Use Splinter to visit every product and scrape for the full Image link.
+    Append the Image Title and Link into a list.
+    '''
+    for imageItem in imageItems:
+        imageSrc = imageItem.find('a', class_='itemLink product-item')['href']
+        title = imageItem.find('div', class_='description').text
+        
+        browser.visit(base_url_hemisphere + imageSrc)
+        enhanced_soup = BeautifulSoup(browser.html, 'html.parser')
+        
+        downloadTag = enhanced_soup.find('div', class_='downloads')
+        enhancedImageSrc = downloadTag.find('a')['href']
+        
+        hemisphere_image_urls.append({
+            "title":title,
+            "img_url":enhancedImageSrc
+        })
+
+    mars_facts['hemispheres'] = hemisphere_image_urls
 
     print("============= Mars Facts ===========================================================================")
     print(f" Head Line : {mars_facts['headline']}")
     print(f" Description : {mars_facts['description']}")
-    print(f"The featured Image URL is : {featured_image_url}")
+    #print(f"The featured Image URL is : {featured_image_url}")
+    print(f"Mars Weather Tweet: {mars_facts['weathertweet']} ")
+    print(f"Mars Facts: {mars_facts['table']}")
+    print(f"hemisphere_image_urls = {pprint.pprint(mars_facts['hemispheres'], indent=1)} ")
     print("============= Mars Facts ===========================================================================")
 
     return  mars_facts
