55cc19581d5296ac6d177f5fc7d6cd1620e4e238
Kaushik Samaddar <kaushiksamaddar.gmail.com>
2019-03-02 16:04:59 -0600

    Updated code for Gauge Chart

--- Interactive Dashboards and Visualizations/StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ Interactive Dashboards and Visualizations/StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -8,8 +8,8 @@ function buildMetadata(sample) {
   // Use `Object.entries` to add each key and value pair to the panel
   // Hint: Inside the loop, you will need to use d3 to append new
   // tags for each key-value in the metadata.
-  d3.json(`/metadata/${sample}`).then( metadata => {
-    Object.entries(metadata).forEach( (entry) => {
+  d3.json(`/metadata/${sample}`).then(metadata => {
+    Object.entries(metadata).forEach((entry) => {
       var para = metaSelector.append("p");
       para.text(`${entry[0]} : ${entry[1]}`);
     });
@@ -18,12 +18,11 @@ function buildMetadata(sample) {
   // buildGauge(data.WFREQ);
 }
 
-function buildCharts(sample) {
-
+function buildBubbleCharts(sample){
   //Use `d3.json` to fetch the sample data for the plots
   console.log(`Build Charts for sample ${sample}`);
 
-  d3.json(`/samples/${sample}`).then( sampleData => {
+  d3.json(`/samples/${sample}`).then(sampleData => {
 
     console.log("Build a Bubble Chart on the Sample Data")
 
@@ -32,16 +31,16 @@ function buildCharts(sample) {
       y: sampleData.sample_values,
       text: sampleData.otu_labels,
       mode: 'markers',
-      marker:{
-        size:sampleData.sample_values,
-        color:sampleData.otu_ids
+      marker: {
+        size: sampleData.sample_values,
+        color: sampleData.otu_ids
       }
     }
 
     var data = [trace_bubble];
 
     var layout = {
-      xaxis: {"title": "OTU ID"}
+      xaxis: { "title": "OTU ID" }
     };
 
     //Build a Bubble Chart using the sample data
@@ -49,29 +48,131 @@ function buildCharts(sample) {
 
   });
 
+}
+
+function buildPieCharts(sample){
   d3.json(`/samples/${sample}`).then(sampleData => {
     console.log("Build a Pie Chart on the Sample Data");
 
     var trace_pie = {
-      labels : sampleData.otu_ids.slice(0, 10),
-      values : sampleData.sample_values.slice(0, 10),
-      hovertext : sampleData.otu_labels.slice(0, 10),
-      type : "pie"
+      labels: sampleData.otu_ids.slice(0, 10),
+      values: sampleData.sample_values.slice(0, 10),
+      hovertext: sampleData.otu_labels.slice(0, 10),
+      type: "pie"
     };
 
     var data_pie = [trace_pie];
 
     //Build a Pie Chart using the sample data.
     Plotly.newPlot('pie', data_pie);
-
-
   });
-
 }
 
 //Construct the Gauge Chart out of the sample
-function buildGauge(sample){
+function buildGaugeCharts(sample) {
 
+  console.log("Build a Gauge Chart on the sample data.")
+  // Use d3.json to fetch the frequency of the Sample.
+  var level = 0;
+
+  d3.json(`/wfreq/${sample}`).then(sampleData => {
+    console.log(`Sample ${sampleData.sample} has a frequency of ${sampleData.WFREQ}`);
+    level = sampleData.WFREQ * 20;
+    console.log(`level is set to ${level}`);
+
+    // Trig to calc meter point
+    var degrees = 180 - level, radius = 0.5;
+    var radians = degrees * Math.PI / 180;
+    var x = radius * Math.cos(radians);
+    var y = radius * Math.sin(radians);
+
+    // Path: may have to change to create a better triangle
+    var mainPath = "M -.0 -0.025 L .0 0.025 L ",
+      pathX = String(x),
+      space = " ",
+      pathY = String(y),
+      pathEnd = " Z";
+    var path = mainPath.concat(pathX, space, pathY, pathEnd);
+
+    var data = [
+      {
+        type: "scatter",
+        x: [0],
+        y: [0],
+        marker: { size: 28, color: "850000" },
+        showlegend: false,
+        name: "speed-marker",
+        text: level,
+        hoverinfo: "text+name"
+      },
+      {
+        values: [50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50],
+        rotation: 90,
+        text: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
+        textinfo: "text",
+        textposition: "inside",
+        marker: {
+          colors: [
+            "rgba(14, 127, 0, .5)",
+            "rgba(110, 154, 22, .5)",
+            "rgba(170, 202, 42, .5)",
+            "rgba(202, 209, 95, .5)",
+            "rgba(210, 206, 145, .5)",
+            "rgba(232, 226, 202, .5)",
+            "rgba(242, 236, 212, .5)",
+            "rgba(252, 246, 222, .5)",
+            "rgba(262, 256, 232, .5)",
+            "rgba(255, 255, 255, 0)"
+          ]
+        },
+        labels: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1"],
+        hoverinfo: "label",
+        hole: 0.5,
+        type: "pie",
+        showlegend: false
+      }
+    ];
+
+    var layout = {
+      shapes: [
+        {
+          type: "path",
+          path: path,
+          fillcolor: "850000",
+          line: {
+            color: "850000"
+          }
+        }
+      ],
+      title: "<b>Gauge</b> <br> Speed 0-100",
+      height: 500,
+      width: 500,
+      xaxis: {
+        zeroline: false,
+        showticklabels: false,
+        showgrid: false,
+        range: [-1, 1]
+      },
+      yaxis: {
+        zeroline: false,
+        showticklabels: false,
+        showgrid: false,
+        range: [-1, 1]
+      }
+    };
+    Plotly.newPlot("gauge", data, layout);
+  });
+}
+
+function buildCharts(sample) {
+  //Construct a Bubble Chart on the sample.
+  buildBubbleCharts(sample);
+
+  //Construct a Pie Chart on the sample.
+  buildPieCharts(sample);
+
+  //Construct a Gauge Chart on the sample.
+  buildGaugeCharts(sample);
 }
 
 function init() {
@@ -82,7 +183,7 @@ function init() {
     sampleNames.forEach((sample) => {
       selector
         .append("option")
-        .text("BB_"+sample)
+        .text("BB_" + sample)
         .property("value", sample);
     });
 
@@ -90,9 +191,6 @@ function init() {
     const firstSample = sampleNames[0];
     buildCharts(firstSample);
     buildMetadata(firstSample);
-
-    //BONUS: Build Gauge Chart
-    buildGauge(newSample);
   });
 }
 
@@ -100,9 +198,6 @@ function optionChanged(newSample) {
   // Fetch new data each time a new sample is selected
   buildCharts(newSample);
   buildMetadata(newSample);
-
-  //BONUS: Build Gauge Chart
-  buildGauge(newSample);
 }
 
 // Initialize the dashboard
